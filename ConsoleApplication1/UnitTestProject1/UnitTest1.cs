1e44c61fd0bda947f17bf09f82f1136462dd3bb8
annekarishma <karishma.anne@gmail.com>
2013-09-16 17:04:53 -0400

    2

--- ConsoleApplication1/UnitTestProject1/UnitTest1.cs
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -1,1 +1,23 @@
-﻿
+﻿public void VehiclesCanTransform()
+{
+var v = new Vehicle();
+v.Transform();
+
+Assert.IsTrue(v.Log.Contains("autobot"));
+}
+
+[TestMethod]
+public void Linq101()
+{
+var numbers = new[] { 0, 1, 2, 3, 4, 5 };
+var actual = new List<int>();
+foreach (var x in numbers)
+{
+if (x % 2 ==0) actual.Add(x);
+}
+Assert.AreEqual(3, actual.Count);
+var actual2 = numbers.Where(i => i % 2 == 0).Reverse();
+Assert.ArweEqual(3, actual2.Count());
+}
+}
+}
